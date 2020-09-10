d2a74b241c7794abee546d1c008c306a55a75cc5
Brawlence <42910943+Brawlence@users.noreply.github.com>
2020-09-10 22:56:18 +0300

    Proof-of-concept finished
    Added opacity transition for header, reddit links detection before conversion them into ajax, scroll to top on page ajax load

--- prereq/ajax_links.js
+++ prereq/ajax_links.js
@@ -7,25 +7,28 @@ request.addEventListener("readystatechange", () => {
     if (request.readyState === 4 && request.status === 200) {
         let responseObj = JSON.parse(request.response);
         let postObj = responseObj[0].data.children[0].data;
-        console.log(postObj);
+        //console.log(postObj);
         document.getElementsByTagName("title")[0].innerHTML = postObj.title;
         document.getElementsByTagName("meta")[3].innerHTML = postObj.url;
         let tempHtml = postObj.selftext_html;
         tempHtml = tempHtml.replace(/&lt;/g, "<");
         tempHtml = tempHtml.replace(/&gt;/g, ">");
         tempHtml = tempHtml.replace(/&amp;nbsp;/g, " ");
-        tempHtml = tempHtml.replace(/<a href=/g, "<a class =\"ajax\" href=");
+        tempHtml = tempHtml.replace(/<a href=([\S]+)reddit/g, "<a class =\"ajax\" href=$1reddit");
+        document.getElementById("header").innerHTML = postObj.title;
+        document.getElementById("whereTo").value = postObj.url;
         document.getElementById("ajaxable").innerHTML = tempHtml;
+        document.getElementById("whereTo").focus();
     }
 });
 
 function initiateShift (e) {
     if (e.target.className === "ajax") {
         e.preventDefault();
-        let url = document.getElementById('whereTo').value + "/.json";//e.target.href + "/.json"; // Somewhy getting JSON is not forbidden by CORS?
-        request.open('GET', url);
+        let url = e.target.href || document.getElementById('whereTo').value; // Somewhy getting JSON is not forbidden by CORS?
+        request.open('GET', url + "/.json");
         //request.setRequestHeader('Content-Type', 'application/x-www-form-url');
-        request.send(); //Не сработает на локальном файле, ибо CORS policy …
+        request.send();
         return false;
     }
 }
